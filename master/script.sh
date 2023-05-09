158c2553e41a193b3689272bf6b96b64bf363a65
Shturman <hexade@yandex.ru>
2023-05-09 20:55:35 +0300

    доработка mysql_master

--- master/script.sh
+++ master/script.sh
@@ -1,1 +1,5 @@
-$MYSQL -e "show master status;";
+MYSQL='mysql -uroot -psuperuser'
+$MYSQL -e "CREATE USER repl@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'superuser';";
+$MYSQL -e "GRANT REPLICATION SLAVE ON *.* TO repl@'%';";
+$MYSQL -e "SHOW MASTER STATUS;";
+$MYSQL -e "SELECT @@server_id;";
