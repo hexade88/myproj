2c9aa884fd0d00b560ac213fe1502713158ab349
Antonov Alexandr <antonov@polymetal.ru>
2023-05-12 17:57:25 +0300

    uu ty tyty

--- db/repl.sh
+++ db/repl.sh
@@ -1,5 +1,10 @@
 MYSQL='mysql -uroot -psuperuser -h127.0.0.1 -P3306'
 $MYSQL -e "CREATE USER repl@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'superuser';";
 $MYSQL -e "GRANT REPLICATION SLAVE ON *.* TO repl@'%';";
-$MYSQL -e "SHOW MASTER STATUS;";
+
+rezult=$($MYSQL -N -e "SHOW MASTER STATUS;";)
+read binlog position <<< $rezult
+echo $binlog
+echo $position
+
 $MYSQL -e "SELECT @@server_id;";
