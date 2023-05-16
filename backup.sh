66f3cc9e4e141f1e63e92f047f18c016230a9830
Antonov Alexandr <antonov@polymetal.ru>
2023-05-16 09:36:47 +0300

    uyyuuyyt yu

--- backup.sh
+++ backup.sh
@@ -12,7 +12,7 @@ $MYSQL -e "STOP SLAVE;";
 echo $?;    #проверка для себя
 for s in `$MYSQL --skip-column-names -e "select distinct(TABLE_SCHEMA) from information_schema.tables where TABLE_SCHEMA not like 'information_schema';"`;
     do
-        mkdir $s;
+        mkdir $HOME/backup/$s;
         for table in `$MYSQL --skip-column-names -e "select table_name from information_schema.tables where TABLE_SCHEMA='$s'";`;
                 do
                         /usr/bin/mysqldump -uroot -psuperuser -h127.0.0.1 -P3308 --source-data --add-drop-table --add-locks --create-options --disable-keys --extended-insert --single-transaction --quick --set-charset --events --routines --triggers $HOME/backup/$s $table
